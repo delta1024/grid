ce4b0a831bb66da67541edd6ae6fb085cc145b4c
Jacob Stannix <jakestannix at gmail.com>
2022-12-15 14:13:15 -0700

    documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,57 @@
-//! A library for dealing with 2D data structures
-//!
-//! This crate provides a 2D data structure called [`Grid`].
-//!
-//! By default [`Grid`] is symetrical, meaning that the grid has the same number of columns on each row.
-//! This can be disabled by using the [`Grid::new_no_symetry`] function.
+/*! A library for dealing with 2D data structures
+
+This crate provides a 2D data structure called [`Grid`].
+By default [`Grid`] is symetrical, meaning that the grid has the same number of columns on each row.
+This can be disabled by using the [`Grid::new_no_symetry`] function.
+
+
+# Examples
+
+## Create a grid from a string (or any iterator of vectors).
+```
+use grid::{Grid, Row};
+
+fn main() {
+
+    let example_grid = "012
+                        345
+                        678";
+    let grid = example_grid
+        .lines()
+        .map(|x| x.trim().chars().map(|x| u32::from(x) - 48).collect::<Vec<u32>>())
+        .collect::<Grid<u32>>();
+    let mut constant_grid: Grid<u32> = Grid::new();
+    constant_grid.push_point((0, 0), 0);
+    constant_grid.push_point((0, 1), 1);
+    constant_grid.push_point((0, 2), 2);
+    constant_grid.push_point((1, 0), 3);
+    constant_grid.push_point((1, 1), 4);
+    constant_grid.push_point((1, 2), 5);
+    constant_grid.push_point((2, 0), 6);
+    constant_grid.push_point((2, 1), 7);
+    constant_grid.push_point((2, 2), 8);
+    assert_eq!(grid, constant_grid);
+
+
+}
+```
+You can do the same thing with a row
+```
+# use grid::Row;
+# fn main() {
+    let mut control = Row::new();
+    control.push('h');
+    control.push('e');
+    control.push('l');
+    control.push('l');
+    control.push('o');
+    let input = "hello";
+    let test = input.chars().collect::<Row<char>>();
+    assert_eq!(control, test);
+# }
+```
+*/
+
 use std::ops::{Add, Deref, DerefMut, Div, Mul, Sub};
 
 #[derive(Debug, Clone)]
@@ -81,7 +129,15 @@ where
 #[derive(Debug, Clone)]
 /// A representation of a row in a grid.
 pub struct Row<T>(Vec<Column<T>>);
-
+impl<T> PartialEq for Row<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+impl<T> Eq for Row<T> where T: Eq {}
 impl<T> Row<T>
 where
     T: Into<Column<T>>,
@@ -90,7 +146,21 @@ where
     pub fn new() -> Self {
         Self(Vec::new())
     }
-    /// Calls push on the underling vector.
+    /** Calls push on the underling vector.
+
+    Grid size will remain consistant if symetry is enabled and you don't manualy extend row 0 in a grid.
+    # Example
+    ```
+    # use grid::{Grid, Column};
+    # fn main() {
+        let mut grid: Grid<i32> = Grid::new();
+        grid.add_row();
+        grid[0].push(3);
+        assert_eq!(grid[0][0], Column::from(3));
+        grid.push_point((0, 0), 2);
+        assert_eq!(grid[0][0], Column::from(2));
+    # }
+    */
     pub fn push(&mut self, value: T) {
         self.0.push(value.into())
     }
@@ -113,6 +183,16 @@ impl<T> DerefMut for Row<T> {
     }
 }
 
+impl<A> FromIterator<A> for Row<A> {
+    fn from_iter<T: IntoIterator<Item = A>>(iter: T) -> Self {
+        Self(
+            iter.into_iter()
+                .map(|x| Column(x))
+                .collect::<Vec<Column<A>>>(),
+        )
+    }
+}
+
 #[derive(Debug, Clone)]
 /// A representation of a 2D data structure.
 pub struct Grid<T> {
@@ -120,6 +200,14 @@ pub struct Grid<T> {
     symetrical: bool,
 }
 
+impl<T> PartialEq for Grid<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.rows == other.rows && self.symetrical == self.symetrical
+    }
+}
 impl<T> Default for Grid<T>
 where
     T: Default + Clone + Into<Column<T>>,
@@ -148,18 +236,43 @@ where
         }
     }
     /** Adds a new row to the grid.
-    if Grid is symetrical resizes the new row to match the length of the other rows.*/
+    if Grid is symetrical resizes the new row to match the length of the other rows.
+    # Example
+    ##
+    ```
+    # fn main() {
+        use grid::Grid;
+        let mut grid: Grid<i32> = Grid::new();
+        let mut x = grid.len();
+        assert_eq!(x, 0);
+        grid.add_row();
+        x = grid.len();
+        assert_eq!(x, 1);
+    # }
+    ```
+    */
     pub fn add_row(&mut self) {
         self.rows.push(Row::new());
         if self.symetrical {
             let len = self.len();
-            let grid_depth = self[0].len();
-            self[len - 1].0.resize(grid_depth, T::default().into())
+            let grid_depth = self.rows[0].len();
+            self.rows[len - 1].0.resize(grid_depth, T::default().into())
         }
     }
     /** Adds column to Grid.
     # Panic
     Panics if called on an asymetrical grid.
+
+    # Example
+    ```
+    # use grid::{Grid, Column};
+    # fn main() {
+        let mut grid: Grid<u32> = Grid::new();
+        grid.push_point((3, 4), 7);
+        grid.add_column();
+        assert_eq!(grid[3][5], Column::from(u32::default()));
+    # }
+    ```
     */
     pub fn add_column(&mut self) {
         if !self.symetrical {
@@ -169,14 +282,31 @@ where
             );
         }
 
-        for row in &mut self[..] {
+        if self.rows.len() == 0 {
+            self.rows.push(Row::new())
+        }
+        for row in &mut self.rows[..] {
             row.push(T::default())
         }
     }
-    /** Places `value` at `point`. Grid will be expanded to accomidate point location if neseccary.
-    # Pannic
+    /** Places `value` at `point`.
+    Point is zero indexed.
+
+    Grid will be expanded to accomidate point location if neseccary.
+
+    # Panic
     Panics if grid is asymerical.
-    */
+
+    # Example
+    ```
+    # fn main() {
+    #   use grid::{Grid, Column};
+        let mut grid = Grid::new();
+        grid.push_point((3, 4), 5);
+        assert_eq!(grid[3][4], Column::from(5));
+    # }
+    ```
+        */
     pub fn push_point(&mut self, point: (usize, usize), value: T) {
         if !self.symetrical {
             panic!(
@@ -187,19 +317,31 @@ where
 
         let (x, y) = point;
         if self.len() == 0 {
-            self.add_row();
+            self.rows.push(Row::new());
         }
         if self[0].len() <= y {
-            for _ in self[0].len()..=(y + 1) {
+            for _ in self[0].len()..=y {
                 self.add_column();
             }
         }
         if self.len() <= x {
-            for _ in self.len()..=(x + 1) {
+            for _ in self.len()..=x {
                 self.add_row();
             }
         }
         self[x][y] = value.into();
+    }
+}
+
+impl<A> FromIterator<Vec<A>> for Grid<A> {
+    fn from_iter<T: IntoIterator<Item = Vec<A>>>(iter: T) -> Self {
+        Self {
+            rows: iter
+                .into_iter()
+                .map(|x| x.into_iter().collect::<Row<A>>())
+                .collect::<Vec<Row<A>>>(),
+            symetrical: false,
+        }
     }
 }
 
@@ -213,16 +355,5 @@ impl<T> Deref for Grid<T> {
 impl<T> DerefMut for Grid<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.rows[..]
-    }
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn push_point() {
-        let mut grid = Grid::new();
-        grid.push_point((3, 4), 5);
-        assert_eq!(grid[3][4], Column(5));
     }
 }
